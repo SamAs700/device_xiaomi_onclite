39891c6705584762ca148b2714c99e4f434c92ac
SamAs700 <you@example.com>
2021-08-10 13:19:53 +0300

    onclite: Fluid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,8 @@
+#
+# Copyright (C) 2021 The fluidOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_onclite.mk
+   $(LOCAL_DIR)/fluid_onclite.mk
--- fluid_onclite.mk
+++ fluid_onclite.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2019-2020 The LineageOS Project
 # Copyright (C) 2021 DerpFest
+# Copyright (C) 2021 Project-Fluid
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,12 +14,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-DERP_BUILDTYPE := Official
+# Inherit some common Fluid stuff.
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_onclite
+PRODUCT_NAME := fluid_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
@@ -30,3 +30,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="onc_eea-user 10 QKQ1.191008.001/V11.0.1.0.QFLEUXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Gapps build
+ifeq ($(WITH_GAPPS),true)
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+endif
+
+IS_PHONE := true
+FLUID_BUILD_TYPE := UNOFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=SamAs700 \
+  ro.fluid.cpu=SDM632
